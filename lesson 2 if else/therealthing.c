ee6c324c5aff7a30bda04518e7ac02dad7a95b43
mihirgupta665 <mihirgupta665@gmail.com>
2025-07-27 23:44:37 +0530

    The Real Things in C

---
+++ lesson 2 if else/therealthing.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int a;
+    char x='C';
+    if(3+2%5)
+    printf("its works\n");
+    if(a=10)
+    printf("this also works\n");
+    if(-5)
+    printf("surprising this also works\n");
+    if(a=0)
+    printf("Lets see whether thsi worls or not\n");
+    if(x)
+    printf("this char also works\n");
+    if(x==67)
+    printf("This is an amazing C\n");
+    printf("WOW");
+    return 0;
+}
