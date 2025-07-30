85ac43fb5cc38f2ec7e22a0b288c35d58c422af4
mihirgupta665 <mihirgupta665@gmail.com>
2025-07-30 19:50:26 +0530

    Understanding GP

---
+++ lesson 3 for loops/GPtill_N.c
@@ -0,0 +1,28 @@
+/*
+// Geometric Progression till N.
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the number of terms:  ");
+    scanf("%d",&n);
+    int G=1;
+    for ( int i =2;i<=n;i++){
+        printf("%d ",G);
+        G*=2;
+    }
+    return 0;
+}
+// print 100 50 25 12.5
+#include<stdio.h>
+int main(){
+    int n;
+    float a=100;
+    printf("Input a number of terms: ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+       printf("%f ",a);
+       a/=2 ;
+    }
+    return 0;
+}
+*/
