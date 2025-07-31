a6e7ab514323b3c6f025e3d127391c50dde21dd0
mihirgupta665 <mihirgupta665@gmail.com>
2025-07-31 23:40:23 +0530

    Pattern Number Square

---
+++ lesson 4 Patternprinting/2_pattern_number_square.c
@@ -0,0 +1,18 @@
+/*
+123
+123 
+123
+*/
+# include<stdio.h>
+int main(){
+    int m;
+    printf("Enter a number m: ");
+    scanf("%d",&m);
+    for(int i =1;i<=m;i++){
+        for (int j =1;j<=m;j++){
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
