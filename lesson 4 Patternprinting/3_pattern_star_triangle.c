a3b8c8d1caa2175a43fcd1ad6bc7ec8fb197055d
mihirgupta665 <mihirgupta665@gmail.com>
2025-07-31 23:41:45 +0530

    pattern star triangle

---
+++ lesson 4 Patternprinting/3_pattern_star_triangle.c
@@ -0,0 +1,19 @@
+/*
+*
+**
+***
+****
+*/
+#include<stdio.h>
+int main(){
+    int m;
+    printf("Enter a number m: ");
+    scanf("%d",&m);
+    for (int i =1;i<=m;i++){
+        for (int j=1;j<=i;j++){
+            printf("#");
+        }
+    printf("\n");
+    }
+    return 0;
+}
