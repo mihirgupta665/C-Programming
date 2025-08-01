dd599d88977ad5738f49deeb0a8483fd8cef2e11
mihirgupta665 <mihirgupta665@gmail.com>
2025-08-01 10:30:48 +0530

    Alternate Triangle

---
+++ lesson 4 Patternprinting/15_alternate_triangle.c
@@ -0,0 +1,24 @@
+/*
+1
+0 1
+1 0 1
+0 1 0 1
+*/
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter a number n: ");
+    scanf("%d",&n);
+    int a;
+    for(int i =1;i<=n;i++){
+        if (i%2!=0) a=1;
+        else a=0;
+        for(int j =1;j<=i;j++){
+            printf("%d",a);
+            if(a==0) a=1;
+            else a=0;
+        }
+        printf("\n");
+    }
+    return 0;
+}
