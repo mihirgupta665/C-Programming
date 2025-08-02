351b230945f19b48bd03bf7c37dd626144e83813
mihirgupta665 <mihirgupta665@gmail.com>
2025-08-02 22:47:32 +0530

    Git Pyramid Ultra Mast

---
+++ lesson 4 Patternprinting/22_number_pyramid_ultramast.c
@@ -0,0 +1,27 @@
+/*
+   1
+  121
+ 12321
+1234321
+*/
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter the no. of rows: ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        int a=i-1;
+        for(int j=1;j<=n-i;j++){
+            printf("  ");
+        }
+        for(int k=1;k<=i;k++){
+            printf("%d ",k);
+        }
+        for(int m=1;m<=i-1;m++){
+            printf("%d ",a);
+            a--;
+        }
+        printf("\n");
+    }
+    return 0;
+}
