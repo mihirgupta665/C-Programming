fb08a2c56c3d8a16b8655f4f6b5d4d1e82583410
mihirgupta665 <mihirgupta665@gmail.com>
2025-08-06 08:18:32 +0530

    Pre_in_post_ccrystal_clear

---
+++ lesson 6 Recusion/14_pre_in_post_crystal_clear.c
@@ -0,0 +1,17 @@
+// this is a program to crystal clear one's recursion concept.
+#include<stdio.h>
+void preinpost(int n){
+    if(n==0) return;
+    printf("Pre %d \n",n);
+    preinpost(n-1);
+    printf("In %d \n",n);
+    preinpost(n-1);
+    printf("Post %d \n",n);
+    return;
+}
+int main(){
+    int n;
+    printf("Enter a number n : ");
+    scanf("%d",&n);
+    preinpost(n);
+}
