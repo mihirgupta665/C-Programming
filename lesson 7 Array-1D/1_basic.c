982822e6c805901d7359724820f4cb20b6d3d322
mihirgupta665 <mihirgupta665@gmail.com>
2025-08-07 09:13:42 +0530

    Intro Array 1D

---
+++ lesson 7 Array-1D/1_basic.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int arr1[5] = {0,1,2,3,4,};
+    int arr2[2];
+    arr2[0]=5;
+    arr2[1]=7;
+    arr2[2]=9;
+    printf("%d \n",arr1[1]);
+    printf("%d",arr2[2]);
+}
